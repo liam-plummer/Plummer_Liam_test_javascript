13bc1a370b9f2d2089a94caa647368bab5d8e59b
Liam <liam.f.plummer@gmail.com>
2020-09-07 09:41:42 +0200

    exo 1-8

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+// Boolean
+let maBoolean = true
+
+// L'Array
+let monArray = []
+
+// L'Objet
+let personne = {
+
+}
+
+// Le String
+let monString = "String"
+
+// L'Integer
+let monInt = 14
+
+// L'objet et sa methode
+// let personne2 = {
+//     nom : "Plummer",
+//     prenom: "Liam",
+//     methode(nom){
+//         console.log(`Bonjour, ${nom}`)
+//     }
+// }
+// personne2.methode("Liam")
+
+// La function
+
+// const multi = (x,y) => {
+//     console.log(x * y)
+// }
+// multi(9,5)
+
+// La condition
+// if (monInt > 8) {
+//     console.log(`${monInt} est supperieure a 8`);
+// }
+// else{
+//     console.log(`${monInt} est inferieure a 8`);
+// }
+
+// La boucle
+// for (let i = 0; i <= 9; i++) {
+//     console.log(i)  
+// }
+
+
+
+// L'objet date
+let date = new Date
+date = date.getDate
+console.log(date)
